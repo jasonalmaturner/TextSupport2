2a3786372552b96a97ee12cb813541a7c292369e
jasonalmaturner@gmail.com <jasonalmaturner@gmail.com>
2015-02-11 21:09:53 -0700

    I don't know

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-index
+var express = require('express'),
+	app = express(),
+	firebase = require('firebase'),
+	bodyParser = require('body-parser'),
+	cors = require('cors')
+	port = 9001;
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, function(){
+	console.log('listening at port ' + port);
+})
